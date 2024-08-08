747a26afe39ca715b0cffd5851018c466c18e004
FemputerTech <mleicht92@gmail.com>
2024-08-08 16:42:51 -0700

    functionality to toggle navbar

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,13 @@
+function toggleNavList() {
+  const navList = document.querySelector(".nav-list");
+
+  if (navList.style.display === "") {
+    navList.style.display = "flex";
+  } else {
+    navList.style.display = "";
+  }
+}
+
 async function sendMessage() {
   const userMessageInput = document.getElementById("message");
   const userMessageContent = userMessageInput.value;
